d970ab390e9d53afb8de0e605a7c25e7d69b3855
fx109138 <1091381572@qq.com>
2017-09-13 22:20:38 +0800

    首次提交

---
+++ src/com/fx/LPServlet.java
@@ -0,0 +1,15 @@
+package com.fx;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "LPServlet",urlPatterns = "/LP")
+public class LPServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
